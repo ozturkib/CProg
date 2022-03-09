8302d483c7a557b0abaf6bd6db801199e9f840c5
Ibrahim Ozturk <ibrozturk@gmail.com>
2022-03-09 18:07:01 +0300

    Create H02_01_MerhabaDunya.c

---
+++ H02_01_MerhabaDunya.c
@@ -0,0 +1,15 @@
+/*	
+	İlk programimiz
+	İslev : Merhaba Dunya mesajinin ekrana yazilmasi
+	Yazar : Ibrahim Ozturk
+	Tarih : 07/03/2022
+*/
+#include "stdio.h"
+
+int main(void){
+	int toplam;
+	toplam = 4 + 5;
+	printf("Merhaba "); //ekrana basma
+	printf("Dunya!");
+  	return 0;
+}

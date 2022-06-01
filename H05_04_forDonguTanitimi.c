46feed740600503b51fcf318df67376b90cde9f2
Ibrahim Ozturk <ibrozturk@gmail.com>
2022-06-01 11:47:31 +0300

    Create H05_04_forDonguTanitimi.c

---
+++ H05_04_forDonguTanitimi.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+int main(void){
+	int sayi;
+	for(sayi = 1; sayi <= 10; sayi++){
+		printf("\nSayi = %d", sayi);
+	}
+
+	/*	
+		Sayi = 1, 2, .... 10
+		Once artirip sonra yazdirdigi icin ve ilk degerin gosterimini atliyor.	
+	*/
+	/*
+	while(++sayi <= 10){
+		printf("\nSayi = %d", sayi);
+	}
+	*/	
+
+}

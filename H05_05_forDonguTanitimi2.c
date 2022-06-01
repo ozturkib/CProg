fe88901df48771acb292200c874f81454d375596
Ibrahim Ozturk <ibrozturk@gmail.com>
2022-06-01 11:48:05 +0300

    Create H05_05_forDonguTanitimi2.c

---
+++ H05_05_forDonguTanitimi2.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+int main(void){
+	/*
+	while(1){	
+	}
+	*/
+	/*
+	for(;;){
+	}
+	*/
+	int sayi;
+	//int sayac = 1;
+	//for(sayi = 30; sayi > 0; sayi-=3){
+	for(sayi = 3; sayi <= 30; sayi+=3){
+		printf("\n%d. Sayi = %d", sayi/3, sayi);
+		//sayac++;
+	}
+}

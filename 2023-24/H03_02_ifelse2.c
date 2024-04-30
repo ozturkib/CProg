0bed16675eeafa4f7810685e9efa5237f9db687e
Ibrahim Ozturk <ibrozturk@gmail.com>
2024-04-30 23:49:25 +0300

    Create H03_02_ifelse2.c

---
+++ 2023-24/H03_02_ifelse2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+    Kullanicidan alinan 2 tamsayinin buyukluk-kucukluk karsilastir
+    masini yapiniz.
+    5 2
+    5 sayisi 2 sayisindan buyuktuur.
+    2 3
+    2 sayisi 3 sayisindan kucuktur.
+    6 6
+    6 sayisi 6 sayisina esittir.
+*/
+
+
+
+
+
+int main(void){
+    int sayi1, sayi2;
+    printf("Lutfen 1. sayiyi giriniz: ");
+    scanf("%d", &sayi1);
+
+    printf("Lutfen 2. sayiyi giriniz: ");
+    scanf("%d", &sayi2);
+
+    if(sayi1 > sayi2)
+        printf("Sayi 1 Sayi 2'den buyuktur.\n");
+    else if(sayi1 < sayi2)
+        printf("Sayi 1 Sayi 2'den kucuktur.\n");
+    else
+        printf("Sayi 1 Sayi 2'ye esittir.\n");
+
+    return 0;
+}

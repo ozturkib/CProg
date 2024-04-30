c386983428987a7b3f5e36ecadaa7b0b7c1ef6f9
Ibrahim Ozturk <ibrozturk@gmail.com>
2024-04-30 23:43:06 +0300

    Create H01_02_EscapeKarakterleri.c

---
+++ 2023-24/H01_02_EscapeKarakterleri.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){ //burasi main blogunun basladigi yer - Hoşgeldiniz
+    // Bolum-1
+    printf("Hosgeldiniz!\nMerhaba,\nBilgisayar Programlama Dersi!\n");
+    printf("------------------------\n");
+    // Bolum-2
+    // Escape character: \t'nin tablo amacli kullanimi
+    printf("ID\t\tIsim\t\tSoyisim\t\tKredi\n");
+    printf("1\t\tAhmet\t\tYilmaz\t\t100\n");
+    printf("20240012\tAhmet Furkan\tDemirbilekbuken\t300000000\n");
+    //Bolum-3
+    printf("Birazdan program baslama sesi duyulacaktir.\n");
+    printf("\a\a\aProgram baslamistir!\n");
+    //Bolum-4
+    // Ekrana kupon kodunun : \MERHABA50/ oldugunu goster!
+    printf("Kupon kodunuz \\MERHABA50/\n");
+    //Bolum-5
+    // Ekrana kupon kodunun : "MERHABA50" oldugunu goster!
+    printf("Kupon kodunuz \"MERHABA50\"\n");
+
+    return 0;
+}
+// Yorum satiri
+// Bu benim ilk C program yazma tecrubem!
+/*
+    Bu benim ilk C program yazma tecrubem!
+    Aslinda bir kere daha denemistim.
+*/
